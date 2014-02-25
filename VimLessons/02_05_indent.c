c6504dd051659f2333b69e77ec679acb9f7dfba6
aramasamy <anbu@klyserv.com>
2014-02-25 06:03:58 +0000

    Vim lessons 1 to 3

---
+++ VimLessons/02_05_indent.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Fix the indenting of the entire file
+// ------------------------------------
+//
+// Solution: gg=G
+
+int main() {
+	int c=0;
+	while((c=getchar())!=EOF) {
+          printf("%d",c);
+          //putchar(c);
+        }
+        printf("%d",c);
+	return 0;
+}
