f11e420ab5ccbe390c36c4304de03ab29f48d52a
anbu <anbu@klyserv.com>
2016-05-24 15:00:15 +0100

    autocommit Tue 24 May 15:00:15 BST 2016

--- GameOfLife/python/attempt2.py
+++ GameOfLife/python/attempt2.py
@@ -42,21 +42,58 @@ class Grid(object):
         else:
             self._xlen=0
 
+        return self
+
     def neighbors_live_count(self, y, x):
         ymin = 0 if y == 0 else y-1
         xmin = 0 if x == 0 else x-1
 
-        xmax = self._xlen - 1 if x == self._xlen - 1 else x - 1
-        ymax = self._ylen - 1 if y == self._ylen - 1 else y - 1
+        xmax = self._xlen - 1 if x == self._xlen - 1 else x + 1
+        ymax = self._ylen - 1 if y == self._ylen - 1 else y + 1
 
+        live_count = 0
+        for x1 in xrange(xmin, xmax+1):
+            for y1 in xrange(ymin, ymax+1):
+                live_count += self._grid[y1][x1]
 
+        live_count -= self._grid[y][x]
+
+        return live_count
+
+    def next_gen_for_cell(self, y, x):
+        live_count = self.neighbors_live_count(y, x)
+
+        if self._grid[y][x]:
+            if live_count in (2, 3):
+                return 1
+            else:
+                return 0
+        else:
+            if live_count == 3:
+                return 1
+            else:
+                return 0
+
+    def next_gen_grid(self):
+        new_grid = []
+        for y in xrange(self._ylen):
+            new_grid_row = []
+            for x in xrange(self._xlen):
+                new_grid_row.append(self.next_gen_for_cell(y, x))
+            new_grid.append(new_grid_row)
+
+        return Grid().init_grid_from(new_grid)
 
 if __name__ == '__main__':
-    grid = Grid(3, 5)
+    grid = Grid(20, 50)
     grid.init_random()
+    while True:
+        os.system('clear')
+        print grid.display()
+        import time
+        time.sleep(.1)
+        grid = grid.next_gen_grid()
 
-    os.system('clear')
-    print grid.display()
 
 #-------------------------------------------------------------------------------
 
@@ -71,9 +108,9 @@ def test_init():
 
 def test_initalize_by_me():
     test_grid = [
-                  [1, 1, 1, 1, 1, 1],
-                  [1, 1, 1, 1, 1, 1],
-                  [1, 1, 1, 1, 1, 1]
+                  [1, 1, 0, 0, 1, 1],
+                  [1, 1, 1, 1, 0, 1],
+                  [1, 0, 0, 0, 0, 1]
               ]
 
     grid=Grid()
@@ -81,5 +118,8 @@ def test_initalize_by_me():
 
     assert grid.size() == (3, 6)
 
+    assert grid.neighbors_live_count(1, 1) == 5
 
-
+    assert grid.next_gen_for_cell(1, 1) == 0
+    assert grid.next_gen_for_cell(2, 2) == 1
+    
