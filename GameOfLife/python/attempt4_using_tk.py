46368a37421d136f8aa1fd4c51474df89609dbb6
anbu <anbu@klyserv.com>
2016-05-24 20:30:02 +0100

    autocommit Tue 24 May 20:30:02 BST 2016

--- GameOfLife/python/attempt4_using_tk.py
+++ GameOfLife/python/attempt4_using_tk.py
@@ -9,11 +9,15 @@ def neigbhours(t):
     y, x = t
     return set(product([y-1, y, y+1], [x-1, x, x+1])) - set([(y,x)])
 
-def should_be_alive(is_alive, neigbhours):
-    if is_alive:
-        return len(neigbhours) in (2,3)
-    else:
-        return len(neigbhours) == 3
+
+def array_to_string(array):
+    def display_row(row):
+        return ''.join([str(c) for c in row])
+
+    row_strings = map(display_row, array)
+
+    return '\n'.join(row_strings)
+
 
 class Grid(object):
 
@@ -42,8 +46,33 @@ class Grid(object):
             viewport.append(row)
         return viewport
 
+    def is_alive(self, location):
+        return location in self._grid
+
+    def should_be_alive(self, location):
+        n = neigbhours(location)
+        if self.is_alive(location):
+            return len(filter(self.is_alive, n)) in (2,3)
+        else:
+            return len(filter(self.is_alive, n)) == 3
+
     def compute_next(self):
         cells_to_consider =  reduce(lambda se, ne: se.union(ne), map(neigbhours, self._grid), set([]))
+        cells_alive_in_next = filter(self.should_be_alive, cells_to_consider)
+        return Grid(cells_alive_in_next)
+
+if __name__ == '__main__':
+    import numpy as np
+    rand_array = np.random.randint(0, 2, (40, 80))
+    rand_array = map(list, rand_array)
+    grid = Grid.init_from(rand_array)
+    while True:
+        grid = grid.compute_next()
+        next_array = grid.display(80, 40 )
+        import os; os.system('clear')
+        print array_to_string(next_array).replace('0', ' ').replace('1', '*')
+
+        import time; time.sleep(0.01)
 
 
 #-------------------------------------------------------------------------------
@@ -59,6 +88,10 @@ def test_grid_cast1():
 
     assert test_grid == grid.display(4, 5)
 
-    print grid.compute_next()
+    next_grid_as_array = grid.compute_next().display(4, 5)
 
-    assert 1 == 2
+    assert next_grid_as_array == [[ 0, 0, 0, 0  ], 
+                 [ 0, 1, 1, 0  ], 
+                 [ 1, 0, 0, 1  ], 
+                 [ 0, 1, 1, 0  ], 
+                 [ 0, 0, 0, 0  ]] 
