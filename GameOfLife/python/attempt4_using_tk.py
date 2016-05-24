ab5ce2f2f0c69d3160ad3d4b03dbfef2b8f0f4d5
anbu <anbu@klyserv.com>
2016-05-24 20:00:17 +0100

    autocommit Tue 24 May 20:00:17 BST 2016

--- GameOfLife/python/attempt4_using_tk.py
+++ GameOfLife/python/attempt4_using_tk.py
@@ -5,14 +5,20 @@ from tkinter import *
 from itertools import product
 
 
-def neighbors(y, x):
+def neigbhours(t):
+    y, x = t
     return set(product([y-1, y, y+1], [x-1, x, x+1])) - set([(y,x)])
-    
+
+def should_be_alive(is_alive, neigbhours):
+    if is_alive:
+        return len(neigbhours) in (2,3)
+    else:
+        return len(neigbhours) == 3
 
 class Grid(object):
 
-    def __init__(self):
-        self._grid = []
+    def __init__(self, grid=None):
+        self._grid = grid
 
     @classmethod
     def init_from(clazz, two_d_array):
@@ -25,19 +31,19 @@ class Grid(object):
                 if two_d_array[y][x]:
                    grid.add((y,x))
 
-        return grid
+        return clazz(grid)
 
+    def display(self, xlen, ylen):
+        viewport = []
+        for y in xrange(ylen):
+            row = []
+            for x in xrange(xlen):
+                row.append(1 if (y,x) in self._grid else 0)
+            viewport.append(row)
+        return viewport
 
-
-
-#        master = Tk()
-# 
-#        w = Canvas(master, width=200, height=100)
-#        w.pack()
-# 
-#        w.create_rectangle(50, 20, 150, 80, fill="#476042")
-# 
-#        mainloop()
+    def compute_next(self):
+        cells_to_consider =  reduce(lambda se, ne: se.union(ne), map(neigbhours, self._grid), set([]))
 
 
 #-------------------------------------------------------------------------------
@@ -51,8 +57,8 @@ def test_grid_cast1():
 
     grid = Grid.init_from(test_grid)
 
-    print grid
+    assert test_grid == grid.display(4, 5)
 
-    print neighbors(3, 2)
+    print grid.compute_next()
 
     assert 1 == 2
