c61867a9cc4630e16b2e25d01dc0a66b638dc9f5
anbu <anbu@klyserv.com>
2016-05-26 00:30:13 +0100

    autocommit Thu 26 May 00:30:13 BST 2016

--- CodeChallenge/ProjectEuler/python/problem012.py
+++ CodeChallenge/ProjectEuler/python/problem012.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import math 
-from itertools import Counter
+from collections import Counter
 
 def read_test_cases():
     number_of_tests=int(raw_input())
@@ -22,14 +22,48 @@ def generate_prime_factors(n):
     if n > 1:
         yield n
 
-def count_factors(n):
-    reduce(lambda mp, x: mp[x] = (mp.get(x, 0)+1), generate_prime_factors(n), {})
+def factor_count(n):
+    if n == 1:
+        return 1
+    exps = map(lambda x: x+1, Counter(generate_prime_factors(n)).values())
+    return reduce(lambda x, v: x*v, exps, 1)
+
+def sum_up_to(n):
+    return n * (n+1) / 2
+
+global_cache = {}
+
+def answer_for(max_factor_count):
+    if global_cache.get(max_factor_count):
+        return global_cache.get(max_factor_count)
+
+    i = max([1] + global_cache.keys())
+
+    while True:
+        sum_up = sum_up_to(i)
+        factor_count_of_sum = factor_count(sum_up)
+
+        if not global_cache.get(factor_count_of_sum-1):
+            mk  = max([0] + global_cache.keys())
+            for k in set(range(factor_count_of_sum)) - set(global_cache.keys()):
+                global_cache[k] = sum_up
+            #print global_cache
+        
+        if factor_count_of_sum > max_factor_count:
+            return sum_up
+        i += 1
 
 if __name__ == '__main__':
-    print list(count_factors(72))
+    for c in read_test_cases():
+        print answer_for(c)
+
 
 #------------------------------------------------------------------------
 
 def test_something():
-    pass
+    assert factor_count(1) == 1
+    assert factor_count(2) == 2
+    assert factor_count(3) == 2
+    assert factor_count(10) == 4
+    assert factor_count(72) == 12
 
