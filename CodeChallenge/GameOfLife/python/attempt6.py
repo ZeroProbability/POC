8d1cacf8b643782b6ca4ec0ea5303777a816503a
anbu <anbu@klyserv.com>
2016-05-25 16:30:15 +0100

    autocommit Wed 25 May 16:30:15 BST 2016

--- CodeChallenge/GameOfLife/python/attempt6.py
+++ CodeChallenge/GameOfLife/python/attempt6.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-
-if __name__ == '__main__':
-    main()
+from tkinter import *
 
 class Grid(object):
 
@@ -13,7 +11,7 @@ class Grid(object):
             self._grid = []
             self._lenx = self._leny = 0
         else:
-            self.grid = grid
+            self._grid = grid
             self._leny = len(grid)
             if self._leny:
                 self._lenx = len(grid[0])
@@ -24,21 +22,74 @@ class Grid(object):
         xmin = 0 if x == 0 else x -1
         xmax = self._lenx - 1 if x == self._lenx - 1 else x + 1
 
+        ymin = 0 if y == 0 else y -1
+        ymax = self._leny - 1 if y == self._leny - 1 else y + 1
+
+        total_live_neighbours = 0
+        for xi in xrange(xmin, xmax + 1):
+            for yi in xrange(ymin, ymax + 1):
+                total_live_neighbours += self._grid[yi][xi]
+
+        total_live_neighbours -= self._grid[y][x]
+
+        if self._grid[y][x]: # is alive
+            return 1 if total_live_neighbours in (2, 3) else 0
+        else:
+            return 1 if total_live_neighbours == 3 else 0
+
     def compute_next_grid(self):
-        new_grid = []
-        for y in self._leny:
-            new_grid_row = []
-            for x in self._lenx:
-                new_grid._row.append(self._compute_next_cell(y, x))
+        new_grid_array = []
+        for y in xrange(self._leny):
+            new_grid_array_row = []
+            for x in xrange(self._lenx):
+                new_grid_array_row.append(self._compute_next_cell(y, x))
+
+            new_grid_array.append(new_grid_array_row)
+
+        return Grid(new_grid_array)
+
+class MyWorld(object):
+
+    def __init__(self):
+        master = Tk()
+        self.w = Canvas(master, width=800, height=600)
+        self.w.pack()
+        mainloop()
+
+    def draw_cell(self, y, x, alive=True):
+        ystart = y * 20
+        xstart = x * 20
+
+        if alive:
+            fill_color = "#55FF55"
+        else:
+            fill_color = "#FF0000"
+        self.w.create_rectangle(xstart, ystart, 20, 20, fill="#55FF55", width=0)
+
 
 
 
 def main():
-    print "hello world"
+    MyWorld()
+    
+
+if __name__ == '__main__':
+    main()
 
 #-------------------------------------------------------------------------------
 
 def test_nothing():
-    grid = Grid([[0,0], [1,1]])
+    test_grid_array =  [
+        [ 0, 0, 0, 0 ], 
+        [ 0, 1, 1, 0 ], 
+        [ 0, 1, 1, 0 ], 
+        [ 0, 1, 1, 0 ], 
+        [ 0, 0, 0, 0 ] 
+    ]
+
+    grid = Grid(test_grid_array)
+    next_grid = grid.compute_next_grid()
+
+    print next_grid._grid
     
     assert 1 == 2
