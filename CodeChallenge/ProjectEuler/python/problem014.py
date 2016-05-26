7d6b9cc9f280af14f8f2d0e70db679971626e207
anbu <anbu@klyserv.com>
2016-05-26 03:00:22 +0100

    autocommit Thu 26 May 03:00:22 BST 2016

--- CodeChallenge/ProjectEuler/python/problem014.py
+++ CodeChallenge/ProjectEuler/python/problem014.py
@@ -3,47 +3,31 @@
 import functools
 import itertools
 
-def memoize(obj):
-    cache = obj.cache = {}
+cache = {1: 1}
 
-    @functools.wraps(obj)
-    def memoizer(*args, **kwargs):
-        key = str(args) + str(kwargs)
-        if key not in cache:
-            cache[key] = obj(*args, **kwargs)
-        return cache[key]
-    return memoizer
-
-def f(n):
-    if n % 2 == 0:
-        return n/2
-    return 3 * n + 1
-
-@memoize
-def chain_len_of(n):
-    if n == 1:
-        return (1, 1)
-    return (n, 1 + chain_len_of(f(n))[1])
 
 def read_test_cases():
     number_of_tests=int(raw_input())
     for _ in xrange(number_of_tests):
         yield int(raw_input())
 
-@memoize
-def max_len_under(n):
-    if n == 1:
-        return (1, 1)
-    for i in xrange(1, n):
-        max_len_under(i)
-    if chain_len_of(n)[1] >= max_len_under(n-1)[1]:
-        return chain_len_of(n)
-    else:
-        return max_len_under(n-1)
+def f(n):
+    if n % 2 == 0:
+        return n/2
+    return 3 * n + 1
+
+def len_chain_f(n):
+    if cache.get(n):
+        return cache[n]
+    next_n = f(n)
+    if cache.get(next_n):
+        cache[n] = 1 + cache[next_n]
+        return cache[n]
+    return 1 + len_chain_f(f(n))
 
 def main():
     for c in read_test_cases():
-        print max_len_under(c)[0]
+        print len_chain_f(c)
 
 if __name__ == '__main__':
     main()
