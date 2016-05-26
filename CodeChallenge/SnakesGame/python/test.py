cb45144aa361f6a2ea5fdb4657c13ee7e67f5e9a
anbu <anbu@klyserv.com>
2016-05-26 15:30:16 +0100

    autocommit Thu 26 May 15:30:16 BST 2016

---
+++ CodeChallenge/SnakesGame/python/test.py
@@ -0,0 +1,19 @@
+# SNAKES GAME
+# Use ARROW KEYS to play, SPACE BAR for pausing/resuming and Esc Key for exiting
+
+import curses
+from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
+from random import randint
+
+
+curses.initscr()
+win = curses.newwin(20, 60, 0, 0)
+win.keypad(1)
+curses.noecho()
+curses.curs_set(0)
+win.border(0)
+win.nodelay(1)
+
+key = win.getch()
+
+curses.endwin()
