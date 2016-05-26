e1ea92c7ce7402c849cd48d1868e9417ed77b913
anbu <anbu@klyserv.com>
2016-05-26 04:00:19 +0100

    autocommit Thu 26 May 04:00:19 BST 2016

--- CodeChallenge/ProjectEuler/python/problem014.py
+++ CodeChallenge/ProjectEuler/python/problem014.py
@@ -3,7 +3,9 @@ import itertools
 
 cache = {1: 1}
 
-max_len_cache = {1: {'len': 1, 'at': 1}}
+max_len_cache = [None] * 5000000  # preallocate
+last_populated = 1
+max_len_cache[1] = ( 1, 1) # len , at
 
 def read_test_cases():
     number_of_tests=int(raw_input())
@@ -25,20 +27,22 @@ def len_chain_f(n):
     return 1 + len_chain_f(f(n))
 
 def max_len_under(n):
-    if max_len_cache.get(n):
-        return max_len_cache[n]['at']
-    max_in_cache = max(max_len_cache.keys())
+    global last_populated
+    if max_len_cache[n]:
+        return max_len_cache[n]
+    max_in_cache = last_populated
     for i in xrange(max_in_cache+1, n+1):
-        if len_chain_f(i) >= max_len_cache[i-1]['len']:
-            max_len_cache[i] = {'len': len_chain_f(i), 'at': i}
+        if len_chain_f(i) >= max_len_cache[i-1][0]:
+            max_len_cache[i] = ( len_chain_f(i), i)
         else:
             max_len_cache[i] = max_len_cache[i-1]
 
+    last_populated = n
     return max_len_cache[n]
 
 def main():
     for c in read_test_cases():
-        print max_len_under(c)['at']
+        print max_len_under(c)[1]
 
 if __name__ == '__main__':
     main()
