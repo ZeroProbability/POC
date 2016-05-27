d34a39319d4b28649be314ac74a5e76226fb5c7a
anbu <anbu@klyserv.com>
2016-05-27 08:30:16 +0100

    autocommit Fri 27 May 08:30:16 BST 2016

--- CodeChallenge/GameOfLife/javascript/attempt2/spec/GameOfLifeSpec.js
+++ CodeChallenge/GameOfLife/javascript/attempt2/spec/GameOfLifeSpec.js
@@ -1,10 +1,10 @@
-describe("Player", function() {
-  var Player = require('../lib/GameOfLife.js');
+describe("GameOfLife", function() {
+  var GameOfLifeTest = require('../lib/GameOfLife.js');
+  var gameOfLife;
 
-  it("should be able to play a Song", function() {
-    expect(player.currentlyPlayingSong).toEqual(song);
+  it("should be able to compute next version of grid", function() {
+    gameOfLife = new GameOfLife([[1,2], [2,3]]);
 
-    expect(player).toBePlaying(song);
   });
 
 });
