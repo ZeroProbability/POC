8ac9400171410e18a653e97fd54fd9e240d0fe41
anbu <anbu@klyserv.com>
2016-05-27 09:30:18 +0100

    autocommit Fri 27 May 09:30:18 BST 2016

--- CodeChallenge/GameOfLife/javascript/attempt2/lib/GameOfLife.js
+++ CodeChallenge/GameOfLife/javascript/attempt2/lib/GameOfLife.js
@@ -8,8 +8,21 @@ function GameOfLife(board) {
 
 GameOfLife.prototype.compute_next_board = function() {
     var x, y;
+    var neighboursCount=0;
+    var newGrid = [];
+
     for (y = 0; y < this.leny; y++) {
         for (x = 0; x < this.lenx; x++) {
+            
+        }
+    }
+
+
+    for (y = 0; y < this.leny; y++) {
+        for (x = 0; x < this.lenx; x++) {
+            if(this.board[y][x]) {
+                neighboursCount++;
+            }
         }
     }
 
--- CodeChallenge/GameOfLife/javascript/attempt2/spec/GameOfLifeSpec.js
+++ CodeChallenge/GameOfLife/javascript/attempt2/spec/GameOfLifeSpec.js
@@ -2,12 +2,24 @@ describe("GameOfLifeSpec", function() {
   var GameOfLife = require('../lib/GameOfLife.js');
   var gameOfLife;
 
-  beforeEach(function() {
-    gameOfLife = new GameOfLife();
-  });
-
   it("should be able to compute next version of grid", function() {
+    gameOfLife = new GameOfLife([    
+            [ false,  false, false, false, false] ,
+            [ false,   true,  true,  true, false] ,
+            [ false,   true,  true,  true, false] ,
+            [ false,  false, false, false, false] 
+            ]);
+
+    expectedOuput = [
+            [ false,  false,  true, false, false] ,
+            [ false,   true, false,  true, false] ,
+            [ false,   true, false,  true, false] ,
+            [ false,  false,  true, false, false] 
+    ];
+
+    expect(gameOfLife.compute_next_board()).toEqual(expectedOuput);
 
   });
 
 });
+
