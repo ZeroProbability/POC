69cd7bc2cd13d75905e5b98769c6ba904e4e5205
anbu <anbu@klyserv.com>
2016-05-27 05:30:12 +0100

    autocommit Fri 27 May 05:30:12 BST 2016

---
+++ CodeChallenge/GameOfLife/python/attempt7.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class Grid(object):
+
+    def __init__(self):
+        pass
+
+class GridView(object):
+
+    def __init__(self, grid):
+        self.grid = grid
+
+#-------------------------------------------------------------------------------
+
+
