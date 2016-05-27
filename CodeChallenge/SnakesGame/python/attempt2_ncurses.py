d46ff7894d559510bed1eefe3c6e14e36eac6ea3
anbu <anbu@klyserv.com>
2016-05-27 04:00:18 +0100

    autocommit Fri 27 May 04:00:18 BST 2016

--- CodeChallenge/SnakesGame/python/attempt2_ncurses.py
+++ CodeChallenge/SnakesGame/python/attempt2_ncurses.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import curses
+import time
+import random
 
 UP = (-1, 0)
 DOWN = (1, 0)
@@ -33,13 +35,32 @@ class Board(object):
 
         self.snake = Snake()
         self.snake_direction = RIGHT
+        self.place_apple_at_random()
 
     def move_snake(self):
-        self.snake.add_head(self.snake_direction)
-        self.snake.pop_tail()
+        new_head = self.snake.add_head(self.snake_direction)
+        if not new_head == self.apple_location:
+            self.snake.pop_tail()
 
     def change_direction(self, new_direction):
         self.snake_direction = new_direction
+
+    def place_apple_at_random(self):
+        all_coordinates = set([])
+        for y in xrange(self.starty + 1, self.starty + self.leny - 2):
+            for x in xrange(self.startx + 1, self.startx + self.lenx -2):
+                all_coordinates.add((y, x))
+        import pdb; pdb.set_trace()
+
+        all_coordinates.difference_update(self.snake.coordinates)
+
+        random_location = random.sample(all_coordinates, 1)
+
+        self.place_apple_at(random_location[0], random_location[1])
+
+
+    def place_apple_at(self, y, x):
+        self.apple_location = (y, x)
 
 
 class BoardView(object):
@@ -47,16 +68,55 @@ class BoardView(object):
     def __init__(self, board):
         self.board = board
 
-    def render(self):
+    def __enter__(self):
         curses.initscr()
-        win = curses.newwin(20, 60, 0, 0)
-        win.keypad(1)
+        window = curses.newwin(self.board.leny, self.board.lenx, 
+                self.board.starty, self.board.startx)
+        window.keypad(1)
         curses.noecho()
         curses.curs_set(0)
-        win.border()
-        win.nodelay(0)
-        key = win.getch()
+        window.border()
+        window.nodelay(1)
+
+        self.window = window
+
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
         curses.endwin()
+
+
+    def render(self):
+        self.window.erase()
+        self.window.border()
+        for c in self.board.snake.coordinates:
+            self.window.addch(c[0], c[1], '#')
+
+    def handle_key(self, key):
+        self.window.addstr( 3, 3, str(key))
+        if key == curses.KEY_UP and not board.snake_direction == DOWN:
+            board.change_direction(UP)
+        elif key == curses.KEY_DOWN and not board.snake_direction == UP:
+            board.change_direction(DOWN)
+        elif key == curses.KEY_RIGHT and not board.snake_direction == LEFT:
+            board.change_direction(RIGHT)
+        elif key == curses.KEY_LEFT and not board.snake_direction == RIGHT:
+            board.change_direction(LEFT)
+        else:
+            pass # just ignore
+
+if __name__ == '__main__':
+    board = Board()
+
+    with BoardView(board) as view:
+        view.render()
+
+        while True:
+            time.sleep(0.1)
+            key = view.window.getch()
+            view.handle_key(key)
+            board.move_snake()
+            view.render()
 
 #--------------------------------------------------------------------------------
 
@@ -71,4 +131,7 @@ def test_board():
     board.move_snake()
     assert board.snake.coordinates == [(10, 5), (10, 6), (10, 7), (9, 7)]
 
+    board.place_apple_at(8, 7)
+    board.move_snake()
+    assert board.snake.coordinates == [(10, 5), (10, 6), (10, 7), (9, 7), (8, 7)]
 
