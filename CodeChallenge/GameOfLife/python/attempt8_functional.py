0fbdaf39f0011508195744ddf91519f75edd351c
anbu <anbu@klyserv.com>
2016-05-29 17:00:18 +0100

    autocommit Sun 29 May 17:00:18 BST 2016

--- CodeChallenge/GameOfLife/python/attempt8_functional.py
+++ CodeChallenge/GameOfLife/python/attempt8_functional.py
@@ -19,26 +19,36 @@ class Cell(object):
             return 'DEAD'
         return 'LIVE'
 
-class LiveCell(Cell):
-    def __init__(self):
-        super(LiveCell, self).__init__(LIVE)
+LiveCell = Cell(LIVE)
+DeadCell = Cell(DEAD)
 
-class DeadCell(Cell):
-    def __init__(self):
-        super(DeadCell, self).__init__(DEAD)
+def compute_next_gen_cell(cell, number_of_neighbours):
+    if number_of_neighbours == 3:
+        return LiveCell
+
+    if number_of_neighbours < 2 or number_of_neighbours > 4:
+        return DeadCell
+
+    return cell
 
 class Grid(object):
     def __init__(self):
         self._live_cells = set()
 
+
     def is_live(self, y, x):
         return (y, x) in self._live_cells
+
 
     def create_cell_at(self, y, x):
         self._live_cells.add((y,x))
 
-    def remove_cell_at(self, y, x):
-        self._live_cells.remove((y,x))
+
+    def get_cell_at(self, y, x):
+        if (y, x) in self._live_cells:
+            return LiveCell
+        return DeadCell
+
 
     def live_neighbours_of(self, y, x):
         subset = set()
@@ -51,36 +61,30 @@ class Grid(object):
 
         return subset
 
+
     def all_neighbours(self):
         superset = set()
         for (y, x) in self._live_cells:
-            for y in xrange(y-1, y+2):
-                for x in xrange(x-1, y+2): 
-                    superset.add((y, x))
+            for yi in xrange(y-1, y+2):
+                for xi in xrange(x-1, y+2): 
+                    superset.add((yi, xi))
 
         return superset
+
 
 def compute_next_grid(grid):
     all_neighbours = grid.all_neighbours()
     new_grid = Grid()
     for (y, x) in all_neighbours:
-        print "considering {}", (y,x)
         live_count = len(grid.live_neighbours_of(y, x))
-        new_cell = compute_next_cell_state(LiveCell() if grid.is_live(y,x) else DeadCell(), live_count)
-        if new_cell.is_live():
+        print "{} {} => {}".format(y, x, live_count)
+        new_cell = compute_next_gen_cell(grid.get_cell_at(y, x), live_count)
+
+        if new_cell == LiveCell:
             new_grid.create_cell_at(y, x)
 
     return new_grid
 
-
-def compute_next_cell_state(cell, number_of_neighbours):
-    if number_of_neighbours == 3:
-        return LiveCell()
-
-    if number_of_neighbours < 2 or number_of_neighbours > 4:
-        return DeadCell()
-
-    return cell
 
 def main():
     print "this works!!!"
@@ -90,18 +94,18 @@ if __name__ == '__main__':
 
 #-------------------------------------------------------------------------------
 
-def test_something():
-    dead_cell = DeadCell()
-    live_cell = LiveCell()
+def test_cell():
+    dead_cell = DeadCell
+    live_cell = LiveCell
 
-    assert dead_cell == compute_next_cell_state(dead_cell, 2)
-    assert live_cell == compute_next_cell_state(dead_cell, 3)
+    assert dead_cell == compute_next_gen_cell(dead_cell, 2)
+    assert live_cell == compute_next_gen_cell(dead_cell, 3)
 
-    assert dead_cell == compute_next_cell_state(dead_cell, 5)
-    assert dead_cell == compute_next_cell_state(dead_cell, 7)
+    assert dead_cell == compute_next_gen_cell(dead_cell, 5)
+    assert dead_cell == compute_next_gen_cell(dead_cell, 7)
 
-    assert live_cell == compute_next_cell_state(live_cell, 2)
-    assert live_cell == compute_next_cell_state(live_cell, 3)
+    assert live_cell == compute_next_gen_cell(live_cell, 2)
+    assert live_cell == compute_next_gen_cell(live_cell, 3)
 
 def test_grid():
 
--- JavaFx/GroovySamples/BasicOps.groovy
+++ JavaFx/GroovySamples/BasicOps.groovy
@@ -1,11 +1,11 @@
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
-import javafx.stage.Stage;
+import javafx.application.Application
+import javafx.scene.Group
+import javafx.scene.Scene
+import javafx.scene.canvas.Canvas
+import javafx.scene.canvas.GraphicsContext
+import javafx.scene.paint.Color
+import javafx.scene.shape.ArcType
+import javafx.stage.Stage
  
 class BasicShapes extends Application {
  
