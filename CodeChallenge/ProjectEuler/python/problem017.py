06e17166a8e18ce315b5563f0c74613c743ae188
anbu <anbu@klyserv.com>
2016-05-31 00:00:15 +0100

    autocommit Tue 31 May 00:00:15 BST 2016

--- CodeChallenge/ProjectEuler/python/problem017.py
+++ CodeChallenge/ProjectEuler/python/problem017.py
@@ -4,17 +4,22 @@
 to_twenty= ['', 'One' , 'Two' , 'Three' , 'Four' , 'Five' , 'Six' , 'Seven' , 'Eight' , 'Nine' , 'Ten' ,
          'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Ninteeen', 'Twenty']
 
-tens = [ '', 'Ten', 'Twenty', 'Thirty', 'Fourty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninghty', 'Hundred']
+tens = [ '', 'Ten', 'Twenty', 'Thirty', 'Fourty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety', 'Hundred']
+
+bigs = ['', 'Thousand', 'Million', 'Billion', 'Trillion']
+
+def handle_to_hundred(i):
+    if i < 20:
+        return to_twenty[i%20]
+    else:
+        return ' '.join([tens[i//10], to_twenty[i%10]])
 
 def handle_to_thousand(i):
-    h = i // 100
-    r = i % 100
-    if r < 20:
-        rs = to_twenty[r]
+    if i < 100:
+        return handle_to_hundred(i)
     else:
-        if r//10 > 0:
-            return ' '.join(tens[r//10], 'Hundred', to_twenty(r%10))
-    return ' '.join([handle_to_20(h), rs])
+        return ' '.join([to_twenty[i//100], 'Hundred', handle_to_hundred(i%100)])
+
 
 def split_into_thousands(n):
     yield n % 1000
@@ -22,9 +27,24 @@ def split_into_thousands(n):
         n = n // 1000
         yield n % 1000
 
+
+def read_test_cases():
+    number_of_tests=int(raw_input())
+    for _ in xrange(number_of_tests):
+        yield int(raw_input())
+
 def main():
-    for c in split_into_thousands(10334342343):
-        print c
+    for tcase in read_test_cases():
+        if tcase == 0:
+            print 'Zero'
+        else:
+            tbigs = bigs[:]
+            next_suffix = tbigs.pop(0)
+            r = []
+            for c in split_into_thousands(tcase):
+                r.insert(' '.join([c, handle_to_thousand(c), next_suffix]))
+                next_suffix = tbigs.pop(0)
+            print ' '.join(r)
 
 
 if __name__ == '__main__':
