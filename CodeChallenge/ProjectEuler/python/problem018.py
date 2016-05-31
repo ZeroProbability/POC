794e4c0335ea62edcfd39b6c8154b48bca4d418a
anbu <anbu@klyserv.com>
2016-05-31 16:30:21 +0100

    autocommit Tue 31 May 16:30:21 BST 2016

--- CodeChallenge/ProjectEuler/python/problem018.py
+++ CodeChallenge/ProjectEuler/python/problem018.py
@@ -6,14 +6,27 @@ def read_test_cases():
     for _ in xrange(number_of_tests):
         number_of_rows=int(raw_input())
         rows=[]
-        for i in number_of_rows:
+        for i in xrange(number_of_rows):
             row=[int(n) for n in raw_input().split()]
             rows.append(row)
         yield rows
 
+def evaluate(case):
+    case = case[::-1]
+    sums = []
+    sum_row = case[0]
+    sums.append(sum_row)
+    for row in case[1:]:
+        new_row=[]
+        for i, n in enumerate(row):
+            new_row.append(n + max(sum_row[i] , sum_row[i+1]))
+        sums.append(new_row)
+        sum_row = new_row
+    return sums[::-1]
+
 def main():
-    print read_test_cases()
-    print "Hello there"
+    for case in read_test_cases():
+        print evaluate(case)[0][0]
 
 if __name__ == '__main__':
     main()
@@ -21,4 +34,6 @@ if __name__ == '__main__':
 #------------------------------------------------------------------------
 
 def test_something():
-    assert 1 == 2
+    case1 = [ [1], [10, 20], [100, 200, 300] ]
+    assert evaluate(case1) == [ [321], [210, 320], [100, 200, 300]]
+
