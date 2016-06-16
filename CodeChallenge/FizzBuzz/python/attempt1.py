8c5fb1905bc3e5742b8fa13932735522d418e99e
anbu <anbu@klyserv.com>
2016-06-16 08:30:21 +0100

    autocommit Thu 16 Jun 08:30:21 BST 2016

---
+++ CodeChallenge/FizzBuzz/python/attempt1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+def main():
+    print "Hello there"
+
+if __name__ == '__main__':
+    main()
+
+#------------------------------------------------------------------------
+
+def test_something():
+    assert 1 == 2
+
