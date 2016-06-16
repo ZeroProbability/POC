71966f3533527cca6acc11d8c665ebfaaa980680
anbu <anbu@klyserv.com>
2016-06-16 19:30:02 +0100

    autocommit Thu 16 Jun 19:30:02 BST 2016

---
+++ CodeKata/Kata1/attempt1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class Passenger(object):
+    def __init__(self, current_floor):
+        pass
+
+class Lift(object):
+    def __init__(self):
+        pass
+
+
+class LiftScheduler(object):
+    def __init__(self):
+        pass
+
+    def create_floor_request(self, current_floor, direction):
+        pass
+
+def main():
+    print "Hello there"
+
+if __name__ == '__main__':
+    main()
+
+#------------------------------------------------------------------------
+
+def test_something():
+    assert 1 == 2
