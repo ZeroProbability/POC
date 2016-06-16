b8922dedc37e25bf0892645dac29ddde099932aa
anbu <anbu@klyserv.com>
2016-06-16 09:00:25 +0100

    autocommit Thu 16 Jun 09:00:25 BST 2016

--- CodeChallenge/FizzBuzz/python/attempt1.py
+++ CodeChallenge/FizzBuzz/python/attempt1.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return 'FizzBuzz'
+    elif n % 3 == 0:
+        return 'Fizz'
+    elif n % 5 == 0:
+        return 'Buzz'
+    return None
+
 def main():
-    print "Hello there"
+    for i in xrange(1, 100):
+        fb = fizzbuzz(i)
+        if fb:
+            yield i, fb
 
 if __name__ == '__main__':
-    main()
+    for x,y in main():
+        print x,y
 
 #------------------------------------------------------------------------
 
-def test_something():
-    assert 1 == 2
+def test_fizzbuzz():
+    assert fizzbuzz(3) == 'Fizz'
+    assert fizzbuzz(5) == 'Buzz'
+    assert fizzbuzz(15) == 'FizzBuzz'
+    assert fizzbuzz(10) == 'Buzz'
+    assert fizzbuzz(9) == 'Fizz'
+    assert fizzbuzz(8) is None
 
