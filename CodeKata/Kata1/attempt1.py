497c2530384c7065d87d80e259f764b762c109c1
anbu <anbu@klyserv.com>
2016-06-17 02:00:43 +0100

    autocommit Fri 17 Jun 02:00:43 BST 2016

--- CodeKata/Kata1/attempt1.py
+++ CodeKata/Kata1/attempt1.py
@@ -14,6 +14,9 @@ class Lift(object):
         self.doors_open = False
         self.action_plan = []
 
+    def str(self):
+        return "{} is {}".format(self.lift_id, self.is_busy())
+
     def is_busy(self):
         return len(self.action_plan) > 0
 
@@ -46,19 +49,33 @@ class Lift(object):
         action_result = self.perform(action.name, *(action.args), **(action.kwargs))
         return action_result
 
-
 class LiftScheduler(object):
     def __init__(self, number_of_lifts):
         self.requests = []
         self.lifts = [Lift(lift_id) for lift_id in xrange(1, number_of_lifts+1)]
 
     def create_floor_request(self, request_init_floor, going_up):
-        first_free_lift = [ lift for lift in self.lifts if not lift.is_busy ][0]
-        first_free_lift
-        return first_free_lift
+        free_lifts = [ lift for lift in self.lifts if not lift.is_busy() ]
+        return_lift = self.lifts[0]
+        if len(free_lifts) > 0:
+            return_lift = free_lifts[0]
+
+        return_lift.select_floor(request_init_floor)
+        return return_lift
 
 def main():
-    print "Hello there"
+    lift_scheduler = LiftScheduler(3)
+
+    lift = lift_scheduler.create_floor_request(2, False)
+
+    while(lift.is_busy()):
+        print lift.perform_next_action()
+
+    lift.select_floor(10)
+
+    while(lift.is_busy()):
+        print lift.perform_next_action()
+
 
 if __name__ == '__main__':
     main()
@@ -82,3 +99,5 @@ def test_schedule_new_passenger():
     lift = lift_scheduler.create_floor_request(3, False)
     assert lift.lift_id == 2
 
+    lift = lift_scheduler.create_floor_request(3, False)
+    assert lift.lift_id == 1
