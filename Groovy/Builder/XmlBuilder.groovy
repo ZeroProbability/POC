5fb2064b5a3bea9e17f1f0ae7fe8ce50c4fc0a6e
anbu <anbu@klyserv.com>
2016-06-18 18:30:02 +0100

    autocommit Sat 18 Jun 18:30:02 BST 2016

---
+++ Groovy/Builder/XmlBuilder.groovy
@@ -0,0 +1,16 @@
+def builder = new groovy.xml.MarkupBuilder()
+
+builder.numbers {
+    description 'Squares and factors of 10..15'
+    
+    for(i in 10..15) {
+        number (value: i, square: i*i) { 
+           for (j in 2..<i) {
+              if(i%j == 0) {
+                  factor(value: j)
+              }
+           }
+        }
+    }
+}
+
