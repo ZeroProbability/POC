5a5af005fa718d971793bac264d884613cf74b10
anbu <anbu@klyserv.com>
2016-06-29 00:30:16 +0100

    autocommit Wed 29 Jun 00:30:16 BST 2016

--- CodeChallenge/KnapSack/python/attempt1.py
+++ CodeChallenge/KnapSack/python/attempt1.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
 import itertools
 
 capacity = 10
 
 weights = [1, 3, 4, 7, 9]
+benefit = [3, 2, 5, 4, 3]
 
 def possibilities():
     for itemcount in xrange(1, len(weights)+1):
-        for i in itertools.combinations(weights, itemcount):
-            yield sum(i), i
+        for i in itertools.combinations(xrange(len(weights)), itemcount):
+            yield i
 
-def filter_capacity(v):
-    total, items = v
-    #print total, items
-    return total <= capacity
+def filter_capacity(indexes):
+    cal_capacity = sum(weights[i] for i in indexes)
+    return cal_capacity <= capacity
+
+def sum_benefit(indexes):
+    benefit_sum = sum(benefit[i] for i in indexes)
+    return benefit_sum, tuple(weights[i] for i in indexes) 
 
 def main():
     dd = itertools.ifilter(filter_capacity, possibilities())
-    print max(dd)
+    ff = itertools.imap(sum_benefit, dd)
+    print max(ff)
 
 if __name__ == '__main__':
     main()
