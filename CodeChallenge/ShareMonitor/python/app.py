36417846a350c6aac73e529104e36d94229802c8
anbu <anbu@klyserv.com>
2016-07-15 12:00:18 +0100

    autocommit Fri 15 Jul 12:00:18 BST 2016

--- CodeChallenge/ShareMonitor/python/app.py
+++ CodeChallenge/ShareMonitor/python/app.py
@@ -19,6 +19,10 @@ def add():
         pass
     return render_template("index.html")
 
+@app.route("/quote")
+def quote():
+    return render_template("quote.html")
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
