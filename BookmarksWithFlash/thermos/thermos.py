02822700e5c92af9fd1dbdf7c098baf33a1af18f
anbu <anbu@klyserv.com>
2016-08-01 03:30:18 +0100

    autocommit Mon  1 Aug 03:30:18 BST 2016

--- BookmarksWithFlash/thermos/thermos.py
+++ BookmarksWithFlash/thermos/thermos.py
@@ -8,6 +8,8 @@ from logging import DEBUG
 
 from forms import BookmarksForm
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 app.logger.setLevel(DEBUG)
 app.config['SECRET_KEY']='''\x04\xd2\t>[-\x06\x06\x1ar%I\xeb\x18\xd6*\x0f~\xae\xa26\x95@\xee'''
