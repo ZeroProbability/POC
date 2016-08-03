ccd6573667f18ec79165b968db287928d72909bf
anbu <anbu@klyserv.com>
2016-08-03 09:42:49 +0100

    broken version of bookmarks

---
+++ BookmarksWithFlash/thermos/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.logger.setLevel(DEBUG)
+app.config['SECRET_KEY']='''\x04\xd2\t>[-\x06\x06\x1ar%I\xeb\x18\xd6*\x0f~\xae\xa26\x95@\xee'''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+db = SQLAlchemy(app)
+
+import models
+import views
---
+++ BookmarksWithFlash/thermos/views.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+
+from flask import render_template, url_for, request, redirect, flash
+
+from forms import BookmarksForm
+from models import User, Bookmark
+
+from . import app, db
+
+#Fake login
+def logged_in_user():
+    return User.query.filter_by(username='anbu').first()
+
+print '====> called once'
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html", new_bookmarks = Bookmark.newest(5))
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    form = BookmarksForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        description = form.description.data
+        bm = Bookmark(user=logged_in_user(), url=url, description=description)
+        #db.session.add(bm)
+        db.session.commit()
+        #app.logger.debug('stored url: {}'.format(url))
+        flash("Stored bookmark '{}'".format(description))
+        return redirect(url_for('index'))
+
+    return render_template("add.html", form=form)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
