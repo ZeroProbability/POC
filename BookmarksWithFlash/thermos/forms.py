1c5e6cce265af0f13814ace2ae1d4c6c2c3dfb84
anbu <anbu@klyserv.com>
2016-08-07 15:00:21 +0100

    autocommit Sun  7 Aug 15:00:21 BST 2016

--- BookmarksWithFlash/thermos/forms.py
+++ BookmarksWithFlash/thermos/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms.fields import StringField
+from wtforms.fields import StringField, PasswordField, BooleanField, SubmitField
 from flask_wtf.html5 import URLField
 from wtforms.validators import DataRequired, url
 
