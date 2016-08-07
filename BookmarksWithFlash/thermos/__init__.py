f8ab3992cb8b9b8f2016b681c842d6a8a07da3eb
anbu <anbu@klyserv.com>
2016-08-07 11:30:16 +0100

    autocommit Sun  7 Aug 11:30:16 BST 2016

--- BookmarksWithFlash/thermos/__init__.py
+++ BookmarksWithFlash/thermos/__init__.py
@@ -3,6 +3,8 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from logging import DEBUG
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
@@ -11,5 +13,4 @@ app.config['SECRET_KEY']='''\x04\xd2\t>[-\x06\x06\x1ar%I\xeb\x18\xd6*\x0f~\xae\x
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
 db = SQLAlchemy(app)
 
-from thermos import models
-from thermos import views
+import models
--- BookmarksWithFlash/views.py
+++ BookmarksWithFlash/views.py
@@ -2,10 +2,10 @@ from datetime import datetime
 
 from flask import render_template, url_for, request, redirect, flash
 
-from forms import BookmarksForm
-from models import User, Bookmark
+from thermos.forms import BookmarksForm
+from thermos.models import User, Bookmark
 
-from . import app, db
+from thermos import app, db
 
 #Fake login
 def logged_in_user():
