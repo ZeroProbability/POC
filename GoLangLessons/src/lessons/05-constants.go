b2d33252c13aac085c12b4ce1685029e5ee4da95
anbu <anbu@klyserv.com>
2017-03-31 00:36:36 +0100

    Adding 05-constats.go

--- GoLangLessons/src/lessons/05-constants.go
+++ GoLangLessons/src/lessons/05-constants.go
@@ -2,12 +2,30 @@ package main
 
 import "fmt"
 
+// You can declare constants like this
 const (
     PI = 3.14
     LANG = "GO"
 )
 
+// integer constants with incrementing values can be delcared like this
+const (
+    A = iota
+    B = iota
+    C = iota
+)
+
+// if we do not define the value, the previous value is copied 
+const (
+    D = iota
+    E
+    F
+)
+
 func main() {
     fmt.Println(PI)
     fmt.Println(LANG)
+
+    fmt.Println(A, B, C)
+    fmt.Println(D, E, F)
 }
