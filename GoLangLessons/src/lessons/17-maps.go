90778e764e8b3d650af58f2610afc18f0f60e2f8
anbu <anbu@klyserv.com>
2017-03-31 19:00:41 +0100

    autocommit Fri 31 Mar 19:00:41 BST 2017

--- GoLangLessons/src/lessons/17-maps.go
+++ GoLangLessons/src/lessons/17-maps.go
@@ -3,5 +3,33 @@ package main
 import "fmt"
 
 func main() {
+    var prefixMap map[string]string
+    prefixMap = make(map[string]string)
 
+    prefixMap["Bob"] = "Mr"
+    prefixMap["Joe"] = "Dr"
+    prefixMap["Mary"] = "Mrs"
+
+    fmt.Println("Before update.... ")
+    for k, v := range prefixMap {
+        fmt.Println(v, k)
+    }
+
+    prefixMap["Mary"] = "Dr"
+    fmt.Println("After update...")
+    for k, v := range prefixMap {
+        fmt.Println(v, k)
+    }
+
+    fmt.Println("After delete...")
+    delete(prefixMap, "Joe")
+    for k, v := range prefixMap {
+        fmt.Println(v, k)
+    }
+
+    if _, exists := prefixMap["Bob"]; exists {
+        fmt.Println("There is a Bob")
+    } else {
+        fmt.Println("There is a no Bob")
+    }
 }
