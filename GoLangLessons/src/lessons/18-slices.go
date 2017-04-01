aaf97f9dd9abec94b424f214540f3f41d6214bbb
anbu <anbu@klyserv.com>
2017-04-01 08:00:28 +0100

    autocommit Sat  1 Apr 08:00:28 BST 2017

--- GoLangLessons/src/lessons/18-slices.go
+++ GoLangLessons/src/lessons/18-slices.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    // 
+    // Create a slice with total capacity 3
     var s []int = make([]int, 3, 5)
 
     fmt.Println(s)
+    fmt.Println(s.(type))
 }
