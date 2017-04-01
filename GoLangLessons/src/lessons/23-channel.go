a1ff91783c53b6b927d2041f49a23444889cae88
anbu <anbu@klyserv.com>
2017-04-01 10:00:31 +0100

    autocommit Sat  1 Apr 10:00:31 BST 2017

--- GoLangLessons/src/lessons/23-channel.go
+++ GoLangLessons/src/lessons/23-channel.go
@@ -6,12 +6,14 @@ import (
 )
 
 func main() {
+    done := make(chan bool)
     go func() {
         fmt.Println("Line 1")
         time.Sleep(3 * time.Second)
         fmt.Println("Line 2")
+        done <- true
     }()
     fmt.Println("Begin ==---------------- ")
-    time.Sleep(6 * time.Second)
+    <-done
     fmt.Println("end   ==---------------- ")
 }
