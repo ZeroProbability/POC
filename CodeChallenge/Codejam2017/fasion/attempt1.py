bb886942622859b3c76cd3bbc0e246a8d7c51177
anbu <anbu@klyserv.com>
2017-04-08 17:30:17 +0100

    autocommit Sat  8 Apr 17:30:17 BST 2017

--- CodeChallenge/Codejam2017/fasion/attempt1.py
+++ CodeChallenge/Codejam2017/fasion/attempt1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import heapq
+O, X, P, E = 'o', 'x', '+', '.'
 
 def main():
     t = int(raw_input())
@@ -23,9 +23,39 @@ def main():
 if __name__ == '__main__':
     main()
 
-def arrange():
+def isvalid(grid, n):
+    for row in xrange(n):
+        o_count = 0
+        for col in xrange(n):
+            if grid[row][col] == O:
+                o_count += 1
+            if o_count > 1: return False
+
+    for col in xrange(n):
+        o_count = 0
+        for row in xrange(n):
+            if grid[row][col] == O:
+                o_count += 1
+            if o_count > 1: return False
+
+def arrange(n, m, ):
+    pass
+
+def parse_input_grid(n, size, data_row):
+    grid = []
+    for row in xrange(n):
+        grid.append([E] * n)
+
+    return grid
+
+def print_grid(grid):
+    for row in xrange(len(grid)):
+        print ''.join(grid[row])
 
 #------------------------------------------------------------------------
 
-def test_arrage():
+def test_valid():
+    grid = parse_input_grid(3, 0, [])
+    print_grid(grid)
+    assert 1 == 2
 
