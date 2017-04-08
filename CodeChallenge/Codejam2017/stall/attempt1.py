2e1c61df559acf60d9d096bbc510215e642f6223
anbu <anbu@klyserv.com>
2017-04-08 05:00:02 +0100

    autocommit Sat  8 Apr 05:00:02 BST 2017

--- CodeChallenge/Codejam2017/stall/attempt1.py
+++ CodeChallenge/Codejam2017/stall/attempt1.py
@@ -4,6 +4,15 @@
 from numba import jit
 import heapq
 
+class MaxHeapObj(object):
+  def __init__(self,val): self.val = val
+  def __lt__(self,other): return self.val > other.val
+  def __eq__(self,other): 
+      if type(other) == MaxHeapObj:
+          return self.val == other.val
+      return self.val == other
+  def __str__(self): return str(self.val)
+
 def main():
     t = int(raw_input())
     for i in xrange(1, t + 1):
@@ -11,7 +20,13 @@ def main():
         print "Case #{}: {}".format(i, prevtidy(s))
 
 def split_list(l):
-    heapq.nlargest(1, l)
+    m = heapq.heappop(l)
+    if (m.val - 1) % 2 == 0:
+        heapq.heappush(l, MaxHeapObj((m.val - 1) / 2))
+        heapq.heappush(l, MaxHeapObj((m.val - 1) / 2))
+    else:
+        heapq.heappush(l, MaxHeapObj((m.val - 1) / 2))
+        heapq.heappush(l, MaxHeapObj((m.val - 1) / 2 + 1))
 
 if __name__ == '__main__':
     main()
@@ -19,5 +34,19 @@ if __name__ == '__main__':
 #------------------------------------------------------------------------
 
 def test_find_untidy_digit():
-    assert split_list() < 0
+    l = [MaxHeapObj(5)]
+    heapq.heapify(l)
+    split_list(l)
+    assert sorted([i.val for i in l]) == [2, 2]
+
+    l = [MaxHeapObj(6)]
+    heapq.heapify(l)
+    split_list(l)
+    assert sorted([i.val for i in l]) == [2, 3]
+    split_list(l)
+    assert sorted([i.val for i in l]) == [1, 1, 2]
+    split_list(l)
+    assert sorted([i.val for i in l]) == [0, 1, 1, 1]
+    split_list(l)
+    assert sorted([i.val for i in l]) == [0, 0, 1, 1]
 
