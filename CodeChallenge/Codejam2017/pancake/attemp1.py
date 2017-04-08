f6eff2027a44277126de6ddc05533aa10f2c5abb
anbu <anbu@klyserv.com>
2017-04-08 01:00:20 +0100

    autocommit Sat  8 Apr 01:00:20 BST 2017

--- CodeChallenge/Codejam2017/pancake/attemp1.py
+++ CodeChallenge/Codejam2017/pancake/attemp1.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from numba import jit
+
 def single_line_tests():
     with open('testcase.txt', 'r') as test_file:
         lines=test_file.read().splitlines()
@@ -8,12 +10,34 @@ def single_line_tests():
         individual_tests=lines[1:]
         return (number_of_tests, individual_tests)
 
+@jit
+def invert(inputStr, i, k):
+    inputList = list(inputStr)
+    for i1 in xrange(i, i + k):
+        if inputList[i1] == '+':
+            inputList[i1] = '-'
+        else:
+            inputList[i1] = '+'
+    return ''.join(inputList)
+
 def flip(inputStr, k):
     l = len(inputStr)
-    for i in xrange(l - k):
-        if(l == 
+    count = 0
+    for i in xrange(0, l - k + 1):
+        if inputStr[i] == '-':
+            count += 1
+            inputStr = invert(inputStr, i, k)
+            print inputStr
+    return count
+
 
 #------------------------------------------------------------------------
+
+def test_invert():
+    assert invert('+++', 0, 3) == '---'
+    assert invert('+-++', 0, 3) == '-+-+'
+    assert invert('---+++', 3, 3) == '------'
+    assert invert('---+++', 4, 1) == '---+-+'
 
 def test_something():
     res = flip('---+-++-', 3)
