8c264483abd26523eee60eee45905bac2a56e3e4
anbu <anbu@klyserv.com>
2017-04-11 12:00:20 +0100

    autocommit Tue 11 Apr 12:00:20 BST 2017

--- CodeChallenge/Codejam2008/1a/attempt1.py
+++ CodeChallenge/Codejam2008/1a/attempt1.py
@@ -5,12 +5,19 @@ import sys
 
 def solve(i, search_engines, queries):
     engs = set(search_engines)
+    switch_count = 0
     for q in queries:
-    return "Case #{}: {} {}".format(i, n + m , n * m)
+        if q in engs:
+            engs.remove(q)
+            if len(engs) == 0:
+                switch_count += 1
+                engs = set(search_engines)
+
+    return "Case #{}: {}".format(i, switch_count)
 
 def run_tests(lines):
-    line_no = 1
-    def next_line():
+    line_no = 0
+    def next_line(line_no = line_no):
         line_no += 1
         return lines[line_no]
 
@@ -26,6 +33,7 @@ def run_tests(lines):
         for j in xrange(query_count):
             queries.append(next_line())
 
+        print search_engines
         solution = solve(i, search_engines, queries)
         yield solution
 
@@ -39,12 +47,44 @@ if __name__ == '__main__':
 
 def test_solve():
     assert solve(1, ['google', 'yahoo'], ['yahoo', 'xyz']) == "Case #1: 0"
+    assert solve(1, ['google', 'yahoo'], ['yahoo', 'google']) == "Case #1: 1"
+    assert solve(1, ['google', 'yahoo'], ['yahoo', 'google', 'google', 'yahoo']) == "Case #1: 2"
 
-def _test_sample():
+def test_sample():
     input_data = """2
-3 4
-4 5
-""".splitlines()
-    expected_output = """Case #1: 7 12
-Case #2: 9 20"""
+5
+Yeehaw
+NSM
+Dont Ask
+B9
+Googol
+10
+Yeehaw
+Yeehaw
+Googol
+B9
+Googol
+NSM
+B9
+NSM
+Dont Ask
+Googol
+5
+Yeehaw
+NSM
+Dont Ask
+B9
+Googol
+7
+Googol
+Dont Ask
+NSM
+NSM
+Yeehaw
+Yeehaw
+Googol
+    """.splitlines()
+    expected_output = """Case #1: 1
+Case #2: 0
+"""
     assert "\n".join(list(run_tests(input_data))) == expected_output
