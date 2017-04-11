8ffdd4be46a1d165deb2ef190f0f7da39e43af26
anbu <anbu@klyserv.com>
2017-04-11 11:30:20 +0100

    autocommit Tue 11 Apr 11:30:20 BST 2017

--- CodeChallenge/Codejam2008/1a/attempt1.py
+++ CodeChallenge/Codejam2008/1a/attempt1.py
@@ -3,15 +3,30 @@
 
 import sys
 
-def solve(i, n, m):
+def solve(i, search_engines, queries):
+    engs = set(search_engines)
+    for q in queries:
     return "Case #{}: {} {}".format(i, n + m , n * m)
 
 def run_tests(lines):
+    line_no = 1
+    def next_line():
+        line_no += 1
+        return lines[line_no]
+
     t = int(lines[0])
     for i in xrange(1, t + 1):
-        input_line = lines[i]
-        n, m = [int(s) for s in input_line.split(" ")] 
-        solution = solve(i, n, m)
+        search_engine_count = int(next_line())
+        search_engines = []
+        for j in xrange(search_engine_count):
+            search_engines.append(next_line())
+
+        query_count = int(next_line())
+        queries = []
+        for j in xrange(query_count):
+            queries.append(next_line())
+
+        solution = solve(i, search_engines, queries)
         yield solution
 
 if __name__ == '__main__':
@@ -23,9 +38,9 @@ if __name__ == '__main__':
 # ------------------------------------------------------------------------------
 
 def test_solve():
-    assert solve(1, 3, 4) == "Case #1: 7 12"
+    assert solve(1, ['google', 'yahoo'], ['yahoo', 'xyz']) == "Case #1: 0"
 
-def test_sample():
+def _test_sample():
     input_data = """2
 3 4
 4 5
