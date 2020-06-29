c2047d3eeb4ada6bc6a0459118814edc7c55c023
anbu <git@anbu.work>
2020-06-29 22:30:21 +0100

    autocommit Mon 29 Jun 22:30:21 BST 2020 from anbu-pavilion-1

--- WebFluxServerClient/src/main/java/uk/anbu/blog/webflux/client/WebfluxClient.java
+++ WebFluxServerClient/src/main/java/uk/anbu/blog/webflux/client/WebfluxClient.java
@@ -11,30 +11,25 @@ import uk.anbu.blog.webflux.server.data.Person;
 
 @SpringBootApplication(scanBasePackages = {"uk.anbu.blog.webflux.client"})
 @Slf4j
-public class WebfluxClient implements CommandLineRunner {
+public class WebfluxClient {
     @Bean
     WebClient getWebClient() {
         return WebClient.create("http://localhost:53797");
     }
 
-//    @Bean
-//    CommandLineRunner demo(WebClient client) {
-//        return args -> client.get()
-//                .uri("/persons")
-//                .accept(MediaType.TEXT_EVENT_STREAM)
-//                .retrieve()
-//                .bodyToFlux(Person.class)
-//                .map(String::valueOf)
-//                .subscribe(log::info);
-//    }
-//
+    @Bean
+    CommandLineRunner demo(WebClient client) {
+        return args -> client.get()
+                .uri("/persons")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .retrieve()
+                .bodyToFlux(Person.class)
+                .map(String::valueOf)
+                .subscribe(log::info);
+    }
+
     public static void main(String[] args) {
         new SpringApplicationBuilder(WebfluxClient.class)
                 .run(args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
     }
 }
