9e5fe88aa7acec850ac717e84e7a4ccfbbadc1d3
anbu <git@anbu.work>
2020-06-29 21:00:24 +0100

    autocommit Mon 29 Jun 21:00:24 BST 2020 from anbu-pavilion-1

--- WebFluxServerClient/src/main/java/uk/anbu/blog/webflux/server/controller/PersonController.java
+++ WebFluxServerClient/src/main/java/uk/anbu/blog/webflux/server/controller/PersonController.java
@@ -1,9 +1,11 @@
 package uk.anbu.blog.webflux.server.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 import uk.anbu.blog.webflux.server.data.PersonDataGenerator;
 
 @RestController
+@AllArgsConstructor
 public class PersonController {
     private final PersonDataGenerator personDataGenerator;
 }
--- WebFluxServerClient/src/main/java/uk/anbu/blog/webflux/server/data/Person.java
+++ WebFluxServerClient/src/main/java/uk/anbu/blog/webflux/server/data/Person.java
@@ -1,4 +1,12 @@
 package uk.anbu.blog.webflux.server.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
 }
